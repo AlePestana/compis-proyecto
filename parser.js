e1a0ad17b4309cfa1388256e0a667cb92b844204
AlePestana <marialepestana@gmail.com>
2021-03-25 15:54:42 -0600

    Add parser

---
+++ parser.js
@@ -0,0 +1,172 @@
+const Parser = require('jison').Parser
+
+const grammar = {
+	lex: {
+		macros: {
+			digit: '[0-9]',
+			digits: '[0-9]+',
+			letter: '[a-zA-Z]',
+			letters: '[a-zA-Z]+',
+			blank: ' ',
+			tab: `\t`,
+			newline: `\n`,
+			whitespace: `\s`,
+		},
+		rules: [
+			['\\s+', '/* skip whitespace */'],
+			['program', "return 'PROGRAM'"],
+			['var', "return 'VAR'"],
+			['int', "return 'TYPE'"],
+			['float', "return 'TYPE'"],
+			['print', "return 'PRINT'"],
+			['if', "return 'IF'"],
+			['else', "return 'ELSE'"],
+
+			['{letter}({letter}|{digit})*', "return 'ID'"],
+			['{digits}\\.{digits}', "return 'CTE_F'"],
+			['{digits}', "return 'CTE_I'"],
+			['\\"({letters}|{digits})+\\"', "return 'CTE_STRING'"],
+
+			['\\<\\>', "return '<>'"],
+			['\\<', "return '<'"],
+			['\\>', "return '>'"],
+			['\\=', "return '='"],
+			['\\*', "return '*'"],
+			['\\/', "return '/'"],
+			['-', "return '-'"],
+			['\\+', "return '+'"],
+			['\\(', "return '('"],
+			['\\)', "return ')'"],
+			['\\{', "return '{'"],
+			['\\}', "return '}'"],
+			['\\:', "return ':'"],
+			['\\;', "return ';'"],
+			['\\,', "return ','"],
+
+			['[{blank}{tab}{newline}{whitespace}]', "return 'WS'"],
+			['$', "return 'EOF'"],
+
+			['.', "return 'error'"],
+		],
+	},
+
+	bnf: {
+		initial: [['program', 'return $1']],
+
+		program: [
+			['PROGRAM ID ; block EOF', '$$ = true'],
+			['PROGRAM ID ; vars block EOF', '$$ = true'],
+		],
+
+		id_helper: [
+			['ID', '$$ = yytext'],
+			['ID , id_helper', '$$ = $1'],
+		],
+
+		var_helper: [
+			['id_helper : TYPE', '$$ = $1'],
+			['id_helper : TYPE , var_helper', '$$ = $6'],
+		],
+
+		vars: [['VAR var_helper ;', '$$ = $3']],
+
+		assignment: [['ID = expression ;', '$$ = $3']],
+
+		factor: [
+			['( expression )', '$$ = $1'],
+			['var_cte', '$$ = $1'],
+			['+ var_cte', '$$ = $1'],
+			['- var_cte', '$$ = $1'],
+		],
+
+		term_helper: [
+			['* factor term_helper', '$$'],
+			['/ factor term_helper', '$$'],
+			['', '$$'],
+		],
+
+		term: [['factor term_helper', '$$']],
+
+		exp_helper: [
+			['+ term exp_helper', '$$'],
+			['- term exp_helper', '$$'],
+			['', '$$'],
+		],
+
+		exp: [['term exp_helper', '$$']],
+
+		expression: [
+			['exp', '$$ = $1'],
+			['exp > exp', '$$ = $1 > $2'],
+			['exp < exp', '$$ = $1 < $2'],
+			['exp <> exp', '$$ = $1 != $2'],
+		],
+
+		wtg_helper: [
+			[', expression wtg_helper', '$$'],
+			[', CTE_STRING wtg_helper', '$$'],
+			['', '$$'],
+		],
+
+		writing: [
+			['PRINT ( expression wtg_helper ) ;', '$$'],
+			['PRINT ( CTE_STRING wtg_helper ) ;', '$$'],
+		],
+
+		condition: [
+			['IF ( expression ) block ;', '$$'],
+			['IF ( expression ) block ; ELSE block ;', '$$'],
+		],
+
+		statute: [
+			['assignment', '$$'],
+			['condition', '$$'],
+			['writing', '$$'],
+		],
+
+		block_hlpr: [
+			['statute block_hlpr', '$$'],
+			['', '$$'],
+		],
+
+		block: [
+			['{ }', '$$'],
+			['{ statute block_hlpr }', '$$'],
+		],
+
+		type: [
+			['INT', '$$ = int'],
+			['FLOAT', '$$ = float'],
+		],
+
+		var_cte: [
+			['ID', '$$ = yytext'],
+			['CTE_F', '$$ = Number(yytext)'],
+			['CTE_I', '$$ = Number(yytext)'],
+		],
+
+		cte_string: [['CTE_STRING', '$$ = yytext']],
+	},
+}
+
+const parser = new Parser(grammar)
+
+// Correct input
+const correct_answer = parser.parse(
+	'program prog1; var id1, id2:float; {id1 = 1.1;}'
+)
+console.log('correct answer --> ' + correct_answer)
+
+// Incorrect input
+const wrong_answer1 = parser.parse(
+	'program 3hi; var id1, id2:float; {id1 = 1.1;}'
+)
+console.log('wrong answer 1 --> ' + wrong_answer1)
+
+const wrong_answer2 = parser.parse('prog someID;')
+console.log('wrong answer 2 --> ' + wrong_answer2)
+
+const wrong_answer3 = parser.parse('program prog3;')
+console.log('wrong answer 3 --> ' + wrong_answer3)
+// program prog1; var id1, id2:float; { }
+// program prog1; var id1, id2:int; {id1 = 1;}
