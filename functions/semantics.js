0e33f79749359933346181791bbc11ce05bb562c
AlePestana <marialepestana@gmail.com>
2021-04-11 23:04:56 -0500

    Basic semantic rules for classes, funcs, and id

--- functions/semantics.js
+++ functions/semantics.js
@@ -12,6 +12,58 @@ add_program_id = (program_id) => {
 	func_directory[program_id] = { type: 'program', ref: new Map() }
 }
 
+add_class_id = (class_id) => {
+	currentFunc = class_id
+	func_directory[class_id] = { type: 'class', ref: new Map() }
+}
+
+add_func_id = (func_id) => {
+	currentFunc = func_id
+	func_directory[func_id] = { type: currentType, ref: new Map() }
+}
+
+set_current_type = (type) => {
+	currentType = type
+}
+
+add_id = (id) => {
+	// Check if id already exists
+	// CHECK WHY HAS MARKS FALSE
+	// ADD SAME LOGIC TO FUNC AND CLASS
+	// console.log(func_directory[currentFunc].ref.has(id))
+	// if (func_directory[currentFunc].ref.has(id)) {
+	// 	console.log('ERROR - Variable already exists')
+	// }
+	func_directory[currentFunc].ref[id] = { type: currentType }
+	console.log(func_directory)
+	console.log(func_directory[currentFunc].ref)
+}
+
+add_id_array = (id, size) => {
+	func_directory[currentFunc].ref[id] = { type: `${currentType}[${size}]` }
+	// console.log('received array with id = ' + id + ' and size of = ' + size)
+	console.log(func_directory[currentFunc].ref)
+}
+
+add_id_matrix = (id, sizeR, sizeC) => {
+	func_directory[currentFunc].ref[id] = {
+		type: `${currentType}[${sizeR}][${sizeC}]`,
+	}
+	// console.log(
+	// 	'received matrix with id = ' +
+	// 		id +
+	// 		' and sizeR of = ' +
+	// 		sizeR +
+	// 		' and sizeC of = ' +
+	// 		sizeC
+	// )
+	console.log(func_directory[currentFunc].ref)
+}
+
+delete_func_directory = function () {
+	func_directory = null
+}
+
 // Example to add id
 // add_id_func_dir = function (id) {
 //func_directory.set(id, {ref: new Map(), type: 'cool'});
@@ -20,13 +72,3 @@ add_program_id = (program_id) => {
 // console.log(func_directory)
 // console.log(func_directory[id].ref)
 // }
-
-add_id = (id) => {
-	func_directory[currentFunc].ref[id] = { type: 'currentType' }
-	console.log(func_directory)
-	console.log(func_directory[currentFunc].ref)
-}
-
-delete_func_directory = function () {
-	func_directory = null
-}
---
+++ tests/funcs.js
@@ -0,0 +1,44 @@
+// Func tests
+
+const parser = require('../parser')
+
+console.log('TEST - Func declaration with parameters')
+const test1 = parser.parse(`
+	program prog1; 
+	void func myFunc1 (var <- int id1; ->) { }
+	main() {}`)
+console.log('--> ' + (test1 ? 'yes :)' : 'no :('))
+
+console.log('\n\nTEST - Func declaration with parameters and vars')
+const test2 = parser.parse(`
+	program prog1; 
+    var <- float y[2]; ->
+	void func myFunc1 (var <- int x; ->)
+    var <- float y; ->
+    { }
+	main() {}`)
+console.log('--> ' + (test2 ? 'yes :)' : 'no :('))
+
+console.log('\n\nTEST - Func declarations with parameters and vars')
+const test3 = parser.parse(`
+	program prog1; 
+    var <- float y[2][3]; ->
+	void func myFunc1 (var <- int x; ->)
+    var <- float y; ->
+    { }
+
+    int func myFunc2 (var <- int z; ->)
+    var <- char k; ->
+    { }
+	main() {}`)
+console.log('--> ' + (test3 ? 'yes :)' : 'no :('))
+
+console.log('\n\nTEST - Func declaration with duplicated parameters and vars')
+const test4 = parser.parse(`
+	program prog1; 
+    var <- float y[2]; ->
+	void func myFunc1 (var <- int x; ->)
+    var <- float x; ->
+    { }
+	main() {}`)
+console.log('--> ' + (test4 ? 'yes :)' : 'no :('))
