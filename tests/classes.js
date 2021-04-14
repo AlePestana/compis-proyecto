41f15e8a7f2a5cd89d7de9f8cee5a6629863bdde
AlePestana <marialepestana@gmail.com>
2021-04-14 17:01:48 -0500

    Add compound type error tests for classes

--- tests/classes.js
+++ tests/classes.js
@@ -60,3 +60,53 @@ const test3 = parser.parse(`
 
   main() {}`)
 console.log('--> ' + (test3 ? 'yes :)' : 'no :('))
+
+console.log('ERROR TEST - Class with attribute of another fake class')
+const program4 = `
+program prog1; 
+
+class Person {
+  attributes <- int x; Animal animal1; ->
+  methods <- 
+    int func one(var <- int x; ->)
+    {
+      return (age - x);
+    }
+  ->
+}
+
+var <- float y[2]; Person person1; ->
+
+void func myFunc1 (var <- int x; ->)
+var <- float y; ->
+{ }
+
+main() {}`
+// const test4 = parser.parse(program4)
+// console.log('--> ' + (test4 ? 'yes :)' : 'no :('))
+
+console.log(
+	'ERROR TEST - Class with method of return type of another fake class'
+)
+const program5 = `
+program prog1; 
+
+class Person {
+  attributes <- int x; ->
+  methods <- 
+    Animal func one(var <- int x; ->)
+    {
+      return (age - x);
+    }
+  ->
+}
+
+var <- float y[2]; Person person1; ->
+
+void func myFunc1 (var <- int x; ->)
+var <- float y; ->
+{ }
+
+main() {}`
+// const test5 = parser.parse(program5)
+// console.log('--> ' + (test5 ? 'yes :)' : 'no :('))
