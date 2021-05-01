2e121090e44ac5441f1c35ade10d2908487435e7
aroquev00 <aroquev00@users.noreply.github.com>
2021-05-01 16:58:46 -0500

    Add opcodes comments

--- functions/opcodes.js
+++ functions/opcodes.js
@@ -1,3 +1,8 @@
+// Operation codes
+// Specifies all the possible operation codes (opcode) that the VM can execute
+// Inputs: does not receive parameters
+// Output: get_opcode (the function that returns the corresponding numeric opcode for a given opcode)
+// Used by: semantics.js
 operations = {
   '+': 1,
   '-': 2,
