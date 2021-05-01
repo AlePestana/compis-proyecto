707892c8121312fac001422f0162714ef65f4821
aroquev00 <aroquev00@users.noreply.github.com>
2021-05-01 17:13:54 -0500

    Add reverse opcodes

---
+++ functions/debug/reverse_opcodes.js
@@ -0,0 +1,31 @@
+// Reverse operation codes
+// Reverses the numeric opcode to string representation for debug
+// Inputs: does not receive parameters
+// Output: get_string_opcode (the function that returns the corresponding numeric opcode for a given opcode)
+// Used by: semantics.js
+operations = {
+  1: '+',
+  2: '-',
+  3: '*',
+  4: '/',
+  5: '<',
+  6: '>',
+  7: '==',
+  8: '!=',
+  9: '&',
+  10: '|',
+  11: '=',
+  12: 'print',
+  13: 'read',
+  14: 'gotoV',
+  15: 'gotoF',
+  16: 'goto',
+}
+
+get_string_opcode = (num_opcode) => {
+  return operations[num_opcode]
+}
+
+console.log(get_string_opcode('1'))
+
+module.exports = get_string_opcode
